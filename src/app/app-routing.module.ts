ca00e9b426b58a677e177b2718d460825fd091aa
Rafi <rafistep98@gmail.com>
2022-04-11 22:13:38 -0400

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from "./register/register.component";
 import { HomeComponent } from "./home/home.component";
-import { AppComponent } from "./login/app.component";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
   {
     path: '',
     component: AppComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   },
   {
     path: 'register',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './login/app.component';
+import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
