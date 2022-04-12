fa985d554c964256cc70ca008e037e333b0c4d29
Rafi <rafistep98@gmail.com>
2022-04-11 22:53:17 -0400

    register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,38 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import { ProductComponent } from './product/product.component';
+import { SupportComponent } from './support/support.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { OrdersPageComponent } from './orders-page/orders-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    ProductComponent,
+    SupportComponent,
+    ProductPageComponent,
+    OrdersPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,6 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registrationValues = [
+    {
+      label: 'Country',
+      value: ''
+    },
+    {
+      label: 'Province',
+      value: ''
+    },
+    {
+      label: 'Address',
+      value: ''
+    },
+    {
+      label: 'Postal Code',
+      value: ''
+    },
+    {
+      label: 'Phone Number',
+      value: ''
+    },
+    {
+      label: 'First Name',
+      value: ''
+    },
+    {
+      label: 'Last Name',
+      value: ''
+    },
+    {
+      label: 'Email',
+      value: ''
+    },
+    {
+      label: 'Username',
+      value: ''
+    },
+    {
+      label: 'Password',
+      value: ''
+    },
+    {
+      label: 'Confirm Password',
+      value: ''
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
